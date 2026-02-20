2dff2b7fbb81d62cf43989c799a3681eb405d606
uppermooninfinity <uppermooninfinity@gmail.com>
2026-02-20 21:18:31 +0530

    Update start.py

--- Oneforall/utils/inline/start.py
+++ Oneforall/utils/inline/start.py
@@ -24,16 +24,16 @@ def private_panel(_):
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-        [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source"),
+            InlineKeyboardButton(text=_["S_B_5"], url="https://t.me/docker_git_bit"),
+        ],
+        [
+            InlineKeyboardButton(text="✦ ᴡєʙ ɢᴧϻєꜱ 🎮✨", url="https://telegram-game-hub.vercel.app"),
         ],
     ]
     return buttons
